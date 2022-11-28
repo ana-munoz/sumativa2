8210fbf1d274732686d4db966bdb52723ae87cb0
amunozfuentes <a.munoz@lifeware.cl>
2022-11-27 21:04:31 -0300

    errores de sintaxis arreglados, aún no funca

--- db.js
+++ db.js
@@ -1,27 +1,25 @@
 const mongoose = require('mongoose');
 
 const {
-
-const MONGO_USERNAME,
-const MONGO_PASSWORD,
-const MONGO_HOSTNAME,
-const MONGO_PORT,
-const MONGO_DB
+    MONGO_USERNAME,
+    MONGO_PASSWORD,
+    MONGO_HOSTNAME,
+    MONGO_PORT,
+    MONGO_DB
 } = process.env;
 
-const optiones = {
- userNewUrlParser: true,
- reconnectTries: Number.MAX_VALUE,
- reconnectInterval: 500,
- connectTimeoutMS: 10000,
+const options = {
+    useNewUrlParser: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500,
+    connectTimeoutMS: 10000,
 };
 
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
-mongoose.connect(url, options).then(function(){
- console.log('MongoDB ir connected');
-
+mongoose.connect(url, options).then( function() {
+    console.log('MongoDB is connected');
 })
- .catch( function(err) {
- console.log(err);
-}
+    .catch( function(err) {
+        console.log(err);
+    })
